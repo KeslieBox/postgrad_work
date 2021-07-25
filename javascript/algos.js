a836b17fa4c02298d38a2c72817bdc89e90bd050
KeslieBox <kesliebox@gmail.com>
2021-07-25 16:33:21 -0500

    areThereDuplicates

--- javascript/algos.js
+++ javascript/algos.js
@@ -12,3 +12,21 @@ function sameFrequency(num1, num2){
   }
 
   sameFrequency(182, 281)
+
+  function areThereDuplicates(...args){
+    // use Set to create a new array without any duplicates
+    return new Set(args).size !== args.length
+  }
+
+  function areThereDuplicates(...args) {
+        let frequencyCounter = {}
+        
+        for (let val in args) {
+            frequencyCounter[args[val]] = (frequencyCounter[args[val]] || 0) + 1
+        }
+        
+        for (let key in frequencyCounter){
+            if (frequencyCounter[key] > 1) return true 
+        }
+        return false
+    }

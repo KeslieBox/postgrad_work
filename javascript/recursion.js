bddb9f84108e5788ac55151f128d6082ed09f95f
KeslieBox <kesliebox@gmail.com>
2021-09-29 15:58:00 -0500

    numberOfSteps

--- javascript/recursion.js
+++ javascript/recursion.js
@@ -39,3 +39,25 @@ var reverseString = function(s) {
     
     return swap(0, s.length - 1)
 };
+
+var numberOfSteps = function(num) {
+    if (num === 0) return 0
+    if (num % 2 === 0){
+        return 1 + numberOfSteps(num/2)
+    } else {
+        return 1 + numberOfSteps(num-1)
+    }     
+};
+
+// function stepsToZero(num, count){
+//     if (num === 0) return count
+//     if (num % 2 === 0){
+//         num = num / 2
+//         count++
+//         return stepsToZero(num, count)
+//     } else {
+//         num = num - 1
+//         count++
+//         return stepsToZero(num, count)
+//     }   
+// }

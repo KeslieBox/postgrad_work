bf878ca045fe79871226c23f70eb1181956fab15
KeslieBox <kesliebox@gmail.com>
2021-10-10 18:27:51 -0500

    searchInsert

--- javascript/binary_search.js
+++ javascript/binary_search.js
@@ -18,3 +18,30 @@ var search = function(nums, target) {
 var search = function(nums, target) {
     return nums.includes(target) ? nums.indexOf(target) : -1  
 };
+
+
+var searchInsert = function(nums, target) {
+    let left = 0
+    let right = nums.length - 1
+    let mid 
+    
+    if (target > nums[right]) return right + 1
+    if (target <= nums[left]) return left
+    
+    while (left <= right){
+        mid = Math.floor((left + right) / 2)
+        if (nums[mid] === target){
+            return mid
+        } else if (nums[mid] < target){
+            if (nums[mid + 1] > target || !nums[mid + 1]) {
+                return mid + 1
+            }
+            left = mid + 1
+        } else if (nums[mid] > target) {
+            if (nums[mid - 1] < target || !nums[mid-1]) {
+                return mid
+            }
+            right = mid - 1
+        }     
+    }    
+};
